2c8d6738c0737ab99a9a80d084133dbf88a97c5a
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2021-10-14 23:49:52 -0300

    prueba

---
+++ DESAFIO HERENCIA INMUEBLE/Desafio herencia/Inmueble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_herencia
+{
+    class Inmueble
+
+    {
+        private String Direccion;
+        private int Metros, ant;
+        private double Precio;
+    }
+}
---
+++ proyectos/Carrito de compras/Carrito de compras/Camisa.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Carrito_de_compras
+{
+    class Camisa
+    {
+        private double precio = 1000;
+        private double _cantCamisas=0;
+        private double _subtotal=0;
+
+        public void agregarCamisa()
+        {
+            _cantCamisas += 1;
+            _subtotal += 1000;
+            Console.WriteLine(" Se agrego una ca,isa al carrito de compras");
+            Thread.Sleep(300);
+            Console.Clear();
+
+
+        }
+
+        public void eliminarCamisa()
+        {
+            _cantCamisas -= 1;
+            _subtotal -= 1000;
+            Console.WriteLine(" Se elimino una ca,isa al carrito de compras");
+            Thread.Sleep(300);
+            Console.Clear();
+
+
+        }
+
+        public void sacarTotalMenu()
+        {
+            double descuento;
+            double total;
+            Console.WriteLine("Cantidad de camisas en el carrito: " + _cantCamisas);
+            Console.WriteLine("Precio por cada prenda: " + precio);
+            
+            Console.WriteLine("El subtotal sin descuento es: " + _subtotal);
+            if(_cantCamisas>2 && _cantCamisas <= 5)
+            {
+
+                descuento = _subtotal * 0.10;
+                total = _subtotal - descuento;
+                Console.WriteLine("El descuento es de 10%");
+                Console.WriteLine("El total con descuento es: "+ total);
+            }else if (_cantCamisas > 5)
+            {
+                descuento = _subtotal * 0.20;
+                total = _subtotal - descuento;
+                Console.WriteLine("El descuento es de 20%");
+                Console.WriteLine("El total con descuento es: " +total);
+            }
+            else
+            {
+
+                total = _subtotal;
+                Console.WriteLine("Sin descuento");
+                Console.WriteLine("El total con descuento es: "+ total);
+            }
+            
+        }
+
+
+
+
+
+
+    }
+   
+
+    
+}
