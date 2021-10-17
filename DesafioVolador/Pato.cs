d10606b9ca38caf3990c9834d71141fc96201283
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2021-10-17 03:15:04 -0300

    v

---
+++ DesafioVolador/Pato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioVolador
+{
+    class Pato: Ivolador 
+    {
+        private int energia;
+
+    }
+}
