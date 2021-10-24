73840a67c65604877a2b510690177a4e49b1ef5c
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2021-10-24 02:13:41 -0300

    subiendo desafio 15 completo

---
+++ Desafio15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList listaElectrodomesticos = new ArrayList();
+
+            Television tvPlana = new Television(42, true);
+            Lavadora electrolux = new Lavadora(10,20,100,"blanco", "A");
+            Television smartTV = new Television(60,true, "negro", "A", 20,100);
+
+            listaElectrodomesticos.Add(tvPlana);
+            listaElectrodomesticos.Add(smartTV);
+            listaElectrodomesticos.Add(electrolux);
+           
+            foreach(Electrodomestico i in listaElectrodomesticos)
+            {
+                
+                i.precioFinal();
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
